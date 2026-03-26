8309d098621a3b9ec0b1b49e995748b62abe8768
iippprofe-ctrl <iippprofe-ctrl@github.com>
2026-03-26 17:03:18 -0400

    Add GitHub Pages deployment workflow

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
@@ -18,13 +18,13 @@ initStore()
     
     ReactDOM.createRoot(rootElement).render(
       <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
           <AuthProvider>
             <CartProvider>
               <App />
             </CartProvider>
           </AuthProvider>
-        </BrowserRouter>
+        </HashRouter>
       </React.StrictMode>,
     );
   })
